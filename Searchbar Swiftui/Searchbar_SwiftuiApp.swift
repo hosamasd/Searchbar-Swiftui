ed94f55ce6e10d43f63d7258a44051b5c9736119
hosam mohamed <hosamelmalt@gmail.com>
2020-11-30 09:35:31 -0800

    Initial Commit

---
+++ Searchbar Swiftui/Searchbar_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Searchbar_SwiftuiApp.swift
+//  Searchbar Swiftui
+//
+//  Created by Hossam on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct Searchbar_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
